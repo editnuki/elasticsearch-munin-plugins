bfd679bc5e9b1cc9edccf8e9c8512c6cafc79d07
editnuki <edit.nuki@gmail.com>
2014-04-07 19:44:16 +0900

    add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'elasticsearch-munin-plugins'
+maintainer       'editnuki'
+maintainer_email 'edit.nuki@gmail.com'
+license          'All rights reserved'
+description      'elasticsearch munin plugins'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rst'))
+version          '0.0.2'
+supports         'centos'
