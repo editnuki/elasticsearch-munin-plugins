7a13899c43d086eec4ee30d79d5fc9c664da9365
editnuki <edit.nuki@gmail.com>
2014-04-08 22:10:04 +0900

    add node plugins

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'edit.nuki@gmail.com'
 license          'All rights reserved'
 description      'elasticsearch munin plugins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rst'))
-version          '0.0.2'
+version          '1.0.0'
 supports         'centos'
